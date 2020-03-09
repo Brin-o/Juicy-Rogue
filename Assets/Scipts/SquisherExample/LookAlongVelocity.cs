dcc6096770a99369915fc2bc47b14cd9496befc1
Brin <brin@zvan.cc>
2020-03-09 23:25:12 +0100

    Started working on enemy logic

--- Assets/Scipts/SquisherExample/LookAlongVelocity.cs
+++ Assets/Scipts/SquisherExample/LookAlongVelocity.cs
@@ -7,7 +7,6 @@ public class LookAlongVelocity : MonoBehaviour
 {
 	public float minVelocity = 0.01f;
 	public new Rigidbody2D rigidbody;
-	private Vector2 moveDirection;
 
 	private void Update ()
 	{
@@ -21,12 +20,6 @@ public class LookAlongVelocity : MonoBehaviour
 		
 		var angle = Vector2.SignedAngle (Vector2.up, rigidbody.velocity);
 		rotation.z = angle;
-
-		/*
-		TODO
-		Rotate
-		DORotate(float toAngle, float duration)
-   		Rotates the target to the given value.*/
 
 		transform.eulerAngles = rotation;
 	}
---
+++ Assets/Scipts/Tweeners/cooldownTimerTween.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class cooldownTimerTween : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void CooldownDone()
+    {
+        // add tween to larger scale! transform.DOScale
+    }
+    void FadeMeOut()
+    {
+        //todo: add me :)
+    }
+}
