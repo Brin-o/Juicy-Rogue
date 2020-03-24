9b7b9d0d4ab8c9dd21a73e35b125e9fc24a50639
Brin <brin@zvan.cc>
2020-03-25 00:11:04 +0100

    Bullet Ammo system implimented.

---
+++ Assets/gameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class gameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //nastavimo target framerate
+        Application.targetFrameRate = 70;
+        DOTween.SetTweensCapacity(350, 100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
